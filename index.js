ecafddebbd50b3516eaa733b7086f6a89937c7bf
Renny <rennymwangi@gmail.com>
2023-07-25 00:17:39 +0930

    Test markdown generation

--- index.js
+++ index.js
@@ -4,13 +4,67 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the description of your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions for your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What are the usage instructions for your project?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines for your project?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What are the test instructions for your project?",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "What is the license type for your project?",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "BSD 3", "None"],
+  },
+  {
+    type: "input",
+    message: "What is the GitHub username for your project?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is the email address for your project?",
+    name: "email",
+  },
+  // Add more questions here...
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, generateMarkdown(data));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", data);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `[${license}](https://opensource.org/licenses/${license})`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+  This project is licensed under the ${license} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+  \n# ${data.title}
+  \n ${renderLicenseBadge(data.license)}
+  \n ## Description
+  \n ${data.description}
+  \n ## Table of Contents
+  \n * [Installation](#installation)
+  \n * [Usage](#usage)
+  \n * [License](#license) 
+  \n * [Contributing](#contributing)
+  \n * [Tests](#tests)
+  \n ## Installation 
+  \n ${data.installation}
+  \n ## Usage
+  \n ${data.usage}
+  \n ## License 
+  \n ${renderLicenseSection(data.license)}
+  \n ${renderLicenseLink(data.license)}
+  \n ## Contributing
+  \n ${data.contribution}
+  \n ## Tests
+  \n ${data.test}
+  `;
 }
 
 module.exports = generateMarkdown;
